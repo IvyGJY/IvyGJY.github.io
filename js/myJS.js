4bd8c8d8ac833aedeeb2c5c4df665b7d8c272601
IvyGJY <595667798@qq.com>
2018-05-01 16:34:07 +0800

    Add files via upload

---
+++ js/myJS.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($){
+
+    $(window).load(function(){
+        $('#spinner').fadeOut(200);
+        $('#prelodad').delay(200).fadeOut('slow');
+        $('.wrapper').fadeIn(200);
+    });
+
+
+})
