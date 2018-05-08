9e222cd1be11d89ac91ce1899480b39922b28c5e
IvyGJY <595667798@qq.com>
2018-05-08 21:39:13 +0800

    Add some effects

--- js/myJS.js
+++ js/myJS.js
@@ -12,9 +12,41 @@ jQuery(document).ready(function($){
         fit: true
     });
 
-    function load_home() {
-        document.getElementById("tech").innerHTML = '<object type="text/html" data="TechBlog.html" width="100%" height="580px"></object>';
-          }
+    // 设置点击菜单的按钮之后 页面弹出的效果
+    var animation_style = 'bounceIn';
+
+    $('.dropdown-select').change(function() {
+        animation_style = $('.dropdown-select').val();
+    });
+
+    $('ul.resp-tabs-list li[class^=tab-]').click(function() {
+
+        var tab_name = $(this).attr('data-tab-name');
+
+        $('.resp-tabs-container').addClass('animated ' + animation_style);
+        $('.resp-tabs-container').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+            $('.resp-tabs-container').removeClass('animated ' + animation_style);
+        });
+
+        $(".content_2").mCustomScrollbar("destroy");
+        $(".content_2").mCustomScrollbar({
+            theme: "dark-2",
+            contentTouchScroll: true,
+            advanced: {
+                updateOnContentResize: true,
+                updateOnBrowserResize: true,
+                autoScrollOnFocus: false
+            }
+        });
+
+        if (tab_name == "FanJour")
+            initialize();
+
+        return false;
+    });
+
+
+
 
 
 })
