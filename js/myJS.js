64346a13f09aa288335d3e18aef501d32ad35b0b
IvyGJY <595667798@qq.com>
2018-06-26 15:01:30 +0800

    some problem

--- js/myJS.js
+++ js/myJS.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($){
 
     $(window).load(function(){
         $('#spinner').fadeOut(200);
-        $('#prelodad').delay(200).fadeOut('slow');
+        $('#preload').delay(200).fadeOut('slow');
         $('.wrapper').fadeIn(200);
     });
 
